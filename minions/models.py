029f402164b8aa758823bd7e13bc6ba452f510f6
linuxyn <1016166567@qq.com>
2018-04-21 09:15:47 +0800

    modify minion table orm

--- minions/models.py
+++ minions/models.py
@@ -6,17 +6,17 @@ from django.db import models
 class Host(models.Model):
     hostname = models.CharField(max_length=64)
     ip = models.GenericIPAddressField(protocol="ipv4")
-    cabinet = models.IntegerField(max_length=32)
+    cabinet = models.IntegerField()
     room = models.CharField(max_length=64)
     cpu_model = models.CharField(max_length=128)
-    cpu_core = models.IntegerField(max_length=128)
-    mem = models.IntegerField(max_length=32)
-    disk = models.IntegerField(max_length=32)
+    cpu_core = models.IntegerField()
+    mem = models.IntegerField()
+    disk = models.IntegerField()
     os = models.CharField(max_length=128)
     product_model = models.CharField(max_length=128)
     business_id = models.ForeignKey("Business")
 
-
+    
 class Business(models.Model):
     name = models.CharField(max_length=64)
     describe = models.CharField(max_length=128)
