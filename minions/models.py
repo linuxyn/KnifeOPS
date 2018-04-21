38b8bf9a0ea63601c0f1c56d7acdda6c1ed00bd4
linuxyn <1016166567@qq.com>
2018-04-21 19:53:53 +0800

    优化了表结构

--- minions/models.py
+++ minions/models.py
@@ -5,29 +5,29 @@ from django.db import models
 
 class Host(models.Model):
     """资产信息表"""
-    hostname = models.CharField(max_length=64, unique=True, null=False)
-    ip = models.GenericIPAddressField(protocol="ipv4", unique=True, null=False)
-    cabinet = models.IntegerField()
-    cpu_model = models.CharField(max_length=128)
-    cpu_core = models.IntegerField()
-    mem = models.IntegerField()
-    disk = models.IntegerField()
-    OS = models.CharField(max_length=128)
-    product_model = models.CharField(max_length=128)
-    minions_status = models.CharField(max_length=32, default="undefined")
-    business = models.ForeignKey("Business", null=False)
-    room = models.ForeignKey("Room", null=False)
+    hostname = models.CharField(max_length=64, unique=True)
+    ip = models.GenericIPAddressField(protocol="ipv4", unique=True)
+    cabinet = models.IntegerField(null=True)
+    cpu_model = models.CharField(max_length=128, null=True)
+    cpu_core = models.IntegerField(null=True)
+    mem = models.IntegerField(null=True)
+    disk = models.IntegerField(null=True)
+    OS = models.CharField(max_length=128, null=True)
+    product_model = models.CharField(max_length=128, null=True)
+    minions_status = models.CharField(max_length=32, null=True, default="undefined")
+    business = models.ForeignKey("Business")
+    room = models.ForeignKey("Room")
 
 
 class Business(models.Model):
     """业务线信息"""
     name = models.CharField(max_length=64)
-    describe = models.CharField(max_length=128)
+    describe = models.CharField(max_length=128, null=True)
 
 
 class Room(models.Model):
     """机房信息表"""
     name = models.CharField(max_length=128)
-    detail = models.CharField(max_length=256)
+    detail = models.CharField(max_length=256,null=True)
 
 
