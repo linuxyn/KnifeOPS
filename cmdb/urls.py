0cea8948eee17678c6948b1fd02e1622c7eb9a39
linuxyn <1016166567@qq.com>
2018-04-22 01:36:20 +0800

    优化了前端页面和后端view函数

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -7,4 +7,5 @@ from cmdb import views
 
 urlpatterns = [
     url(r'hosts/', views.hosts, name="hosts"),
+    url(r'group/', views.group, name="group"),
 ]
--- cmdb/views.py
+++ cmdb/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from cmdb import models
+from django.forms import Form
+from django.forms import widgets
+from django.forms import fields
 
 # Create your views here.
 
 
+class MyForm(Form):
+    group = fields.CharField(
+        required=True,
+        error_messages={"required": "Group name cannot be empty"},
+        max_length=64,
+        label="group"
+    )
+    describe = fields.CharField(
+        required=True,
+        error_messages={"required": "Group name cannot be empty"},
+        max_length=128,
+        label="describe"
+    )
+
+
 def hosts(requests):
     return render(requests, "cmdb/hosts.html")
+
+
+def group(requests):
+    obj = MyForm()
+    group_info = models.Group.objects.all()
+    return render(requests, "cmdb/group.html", {"group_info": group_info, "group_obj": obj})
